a218614aa72048718278a797c6248f06254e1b7b
Vovo4ka322 <142627576+Vovo4ka322@users.noreply.github.com>
2025-08-09 20:22:51 +0300

    Added camera zoom

--- Assets/_Project/Scripts/Installers/MainSceneInstaller.cs
+++ Assets/_Project/Scripts/Installers/MainSceneInstaller.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using UnityEngine;
 using Zenject;
 
@@ -11,6 +12,13 @@ public class MainSceneInstaller : MonoInstaller
     [Header("Scene References")]
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private Grid _gridComponent;
+    
+    [Header("Zoom Settings")]
+    [SerializeField] private CinemachineFreeLook _cinemachineFreeLookCamera;
+    [SerializeField] private float _zoomSensitivity;
+    [SerializeField] private float _zoomSpeed;
+    [SerializeField] private float _minZoom;
+    [SerializeField] private float _maxZoom;
 
     public override void InstallBindings()
     {
@@ -21,6 +29,9 @@ public class MainSceneInstaller : MonoInstaller
         var tileRotator = Bind(new TileRotateController());
         var tileSpawn = Bind(new TileSpawnController(tilePreview, tileRotator, gridBuilder, cameraController));
         var hexInitializer = Bind(new HexGridInitializer(gridBuilder, tileSpawn, hexDirection));
+        var mouseWheelZoomInput = Bind(new MouseWheelZoomInput(_zoomSensitivity));
+        //var TouchPinchZoomInput = Bind(new TouchPinchZoomInput(_zoomSensitivity)); - это под телефоны
+        var cinemachineFreeLookCamera = Bind(new CameraZoomController(_cinemachineFreeLookCamera, mouseWheelZoomInput, _zoomSpeed, _minZoom, _maxZoom));
     }
 
     private T Bind<T>(T controller) where T : class
